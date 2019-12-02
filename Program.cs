611f8b721994ec1b76a2b978652c788bf30b74fa
Deepak-Byte <56863853+Deepak-Byte@users.noreply.github.com>
2019-12-02 06:17:57 -0800

    subham12019

--- Program.cs
+++ Program.cs
@@ -1,114 +1,63 @@
 ﻿using System;
+using NEW_OPPS_PROJECT.Inventorymanagement;
+using NEW_OPPS_PROJECT.Inventorymanager;
+using NEW_OPPS_PROJECT.Stockaccount;
+using NEW_OPPS_PROJECT.StockAccount;
 
-namespace Algorithm_Programs
+
+namespace NEW_OPPS_PROJECT
 {
     class Program
-    {  
-        static void Main(string[] args)
+    {
+        public static void Main(string[] args)
         {
-        
-            Label:
             try
             {
-
                 bool flag = true;
                 while (flag)
                 {
-                    Console.WriteLine("want operation you want to perform");
-                    Console.WriteLine("1:  Permutations of string  ");
-                    Console.WriteLine("2:  Binary Search the Word from Word List ");
-                    Console.WriteLine("3:  Insertion Sort   ");
-                    Console.WriteLine("4:  Bubble Sort ");
-                    Console.WriteLine("5:  Merge Sort  ");
-                    Console.WriteLine("6:  An Anagram Detection Example ");
-                    Console.WriteLine("7:  Prime numbers range ");
-                    Console.WriteLine("8:  Prime numbers range anagram ");
-                    Console.WriteLine("9:  Rewrite Use Generics for Search and Sort Algorithms ");
-                    Console.WriteLine("10: Question to find your number ");
-                    Console.WriteLine("11: Completion time overshoots ");
-                    Console.WriteLine("12: Message Demonstration using String Function and RegEx ");
-                    Console.WriteLine("13: PRESS 13 TO EXIT");
-                    int n = Convert.ToInt32(Console.ReadLine());
-                    switch (n)
+                    Console.WriteLine("--------------HERE IS OPPS PROJECT----------------");
+                    Console.WriteLine("1: INVENTORY MANAGAMENT");
+                    Console.WriteLine("2: INVENTORY MANAGARr CACULATOR");
+                    Console.WriteLine("3: STOCK ACCOUNT MANAGAR");
+                    Console.WriteLine("4: FOR PRINT THE ITEM IN LIST");
+                    Console.WriteLine("5: TO EXIT THE PROGRAM");
+                    int choice = int.Parse(Console.ReadLine());
+                    switch (choice)
                     {
                         case 1:
-                            Permutaionofstring permu = new Permutaionofstring();
-                            permu.PermutaionofString();
-
+                            Inputoutput inputoutput = new Inputoutput();
+                            inputoutput.Print();
                             break;
-
                         case 2:
-                            BinarysearchwordList binarysear = new BinarysearchwordList();
-                            binarysear.binarySearchwordList();
-
+                            Inputoutputmanager inputoutputmanager = new Inputoutputmanager();
+                            inputoutputmanager.Printcalculatvalue();
                             break;
-
                         case 3:
-                             Insertionsortstring insersort = new Insertionsortstring();
-                             insersort.insertionSortstring();
-                             break;
-
+                            Inputstock Inputstock = new Inputstock();
+                            Inputstock.Addnewshare();
+                            break;
                         case 4:
-                            Bubblesort bubblesort = new Bubblesort();
-                            bubblesort.bubbleSort();
+                            UserStockAccount obj = new UserStockAccount();
+                            obj.Stockaccountinput();
                             break;
-
                         case 5:
-                            Mergesort mergee = new Mergesort();
-                            mergee.mergeSort();
-                            break;
-
-                        case 6:
-                            Anagramdetection anagram = new Anagramdetection();
-                            anagram.anagramDetection();
-                            break;
-
-                        case 7:
-                            Primenumrange prinum = new Primenumrange();
-                            prinum.primenumRange();
-                            break;
-
-                        case 8:
-                            Primepalindrom primeanagram = new Primepalindrom();
-                            primeanagram.primenumRangeanagram();
-                            break;
-
-                        case 9:
                             flag = false;
                             break;
-
-                        case 10:
-                            flag = false;
-                            break;
-
-                        case 11:
-                            flag = false;
-                            break;
-
-                        case 12:
-                            flag = false;
-                            break;
-
-                        case 13:
-                            flag = false;
-                            break;
-
                         default:
-                            Console.WriteLine("WRONG INPUT PLEASE ENTER CORRECT INPUT");
+                            Console.WriteLine("wrong input intered");
                             break;
+
                     }
                 }
-
             }
-            catch (FormatException format)
+            catch (FormatException FormatException)
             {
-                Console.WriteLine(format.Message);
-                goto Label;
+                Console.WriteLine(FormatException.Message);
             }
-            catch (Exception e2)
+            catch (Exception Exception)
             {
-                Console.WriteLine(e2.Message);
-                goto Label;
+                Console.WriteLine(Exception.Message);
             }
         }
     }
